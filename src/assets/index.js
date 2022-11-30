cc4b6646a8dfd859771dc70c4198ca0f06365ab7
zaq <85076901+zaq99@users.noreply.github.com>
2022-12-01 04:05:46 +0700

    update logo title

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,9 @@
 import iconUser from './icons-user.png'
 import logoAyoBantu from './logoAyoBantu.png'
+import titleLogo from './titleLogo.svg'
 
 export {
     iconUser, 
     logoAyoBantu,
+    titleLogo,
 }
